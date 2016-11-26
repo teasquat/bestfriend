ffc7d5f490bffdb06ba7f75b1d76e3d961557904
nilq <i.am@nilq.dk>
2016-11-26 15:40:45 +0100

    Pettt

--- src/entities/pet.lua
+++ src/entities/pet.lua
@@ -13,22 +13,24 @@ function pet_factory.make(x,y)
     frcy = 1.5,  -- friction y
     -- static
     g = 30 -- gravity
-
   }
+
   function pet:load()
     world:add(self,self.x,self.y,self.w,self.h)
   end
+
   function pet:update(dt)
     self.dy = self.dy + self.g * dt
     self.dy = self.dy - (self.dy / self.frcy) * dt
 
     self.x, self.y, self.cols = world:move(self, self.x + self.dx, self.y + self.dy)
-
   end
+
   function pet:draw()
     love.graphics.setColor(130, 90, 30)
     love.graphics.rectangle("fill", self.x, self.y, self.w, self.w)
   end
+
   return pet
 end
 return pet_factory
